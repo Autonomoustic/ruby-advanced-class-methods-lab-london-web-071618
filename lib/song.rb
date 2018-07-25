07fda8db817362c70f6163a7b88097f74284c95e
ali aziz <aa_connect@outlook.com>
2018-07-25 07:50:24 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,11 +16,13 @@ class Song
     song
   end
   
-  def self.new_by_name(name)
+  def self.new_by_name
     song = self.new
     song.name = name
     return song
   end
+  
+  def self.create_by_name
     
 
 end
